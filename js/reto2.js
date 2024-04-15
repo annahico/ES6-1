f00ebfde1495926a0e38836b5e9ebc9524354bb2
annahico <annahico@gmail.com>
2024-04-15 21:26:54 +0200

    finalizando reto 2

---
+++ js/reto2.js
@@ -0,0 +1,17 @@
+// class Password {
+//     constructor(longitud, contraseña){
+//         this.longitud = longitud;
+//         this.contraseña = contraseña;
+//     }
+
+const caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+console.log(caracteres.length);
+console.log(caracteres.charAt(2));
+
+Jk08nDvX
+
+0 6 8 19 66 3
+
+minimoIndice = 0
+maximoIndice = caracteres.length -1
