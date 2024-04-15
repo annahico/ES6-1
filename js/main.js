e8ecf4bbf94bbda41b8384edac4230ca99f3a10e
annahico <annahico@gmail.com>
2024-04-15 18:48:24 +0200

    empezando a aprender el constructor

---
+++ js/main.js
@@ -0,0 +1,12 @@
+class Tarea {
+    constructor(texto, id) {
+        console.log("tarea instanciada! con el texto:", texto);
+        this.texto = texto;
+        this.id = id;
+        this.fecha = new Date();
+    }
+
+    fechaHoy() {
+        this.fecha = new Date();
+    }
+}
