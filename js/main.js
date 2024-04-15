3c33f405c0ab178b1bda6adea0f18aacf3a23f0b
annahico <annahico@gmail.com>
2024-04-15 18:58:10 +0200

    aprendiendo concepto THIS

--- js/main.js
+++ js/main.js
@@ -10,3 +10,17 @@ class Tarea {
         this.fecha = new Date();
     }
 }
+//////////////////////////////////////////
+
+//definición de la clase
+class Tarea {
+    constructor(texto) {
+        console.log("construct");
+        this.texto = "hola";
+    }
+}
+
+//crear las instancias de la clase
+let tarea1 = new Tarea(); //los parentesis hacen referencia al constructor
+console.log(tarea1.texto);
+let tarea2 = new Tarea();
