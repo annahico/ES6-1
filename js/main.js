d9a961609310f837415c31dc0c55f7b33462d9a5
annahico <annahico@gmail.com>
2024-04-15 19:17:09 +0200

    fechas

--- js/main.js
+++ js/main.js
@@ -6,21 +6,29 @@ class Tarea {
         this.fecha = new Date();
     }
 
-    fechaHoy() {
+    fechaHoy() {  
         this.fecha = new Date();
+    }
+
+    mostrarFecha() {
+        console.log(this.fecha);
     }
 }
 //////////////////////////////////////////
 
 //definición de la clase
 class Tarea {
-    constructor(texto) {
-        console.log("construct");
-        this.texto = "hola";
+    constructor(textoDeLaVariableOParametro) {
+        console.log("se ha construido");
+        this.texto = texto; //este texto no tiene nada que ver con el del constructor
+        this.id = id;
+        this.fecha = new Date(); //se genera de manera automatica
     }
 }
 
 //crear las instancias de la clase
-let tarea1 = new Tarea(); //los parentesis hacen referencia al constructor
-console.log(tarea1.texto);
-let tarea2 = new Tarea();
+let tarea1 = new Tarea("Comprar leche", 1); //los parentesis hacen referencia al constructor
+let tarea2 = new Tarea("Comprar huevos", 2);
+
+console.log(tarea1);
+console.log(tarea2);
